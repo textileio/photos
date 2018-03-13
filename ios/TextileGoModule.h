73e4467ba61f790d1ed269a433c27f72bd736fbc
Aaron Sutula <aaron@textile.io>
2018-03-13 15:58:50 -0600

    Create a RN native module that wraps the Mobile.framework

---
+++ ios/TextileGoModule.h
@@ -0,0 +1,14 @@
+//
+//  TextileGoModule.h
+//  TextilePhotos
+//
+//  Created by Aaron Sutula on 3/13/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface TextileGoModule : NSObject <RCTBridgeModule>
+
+@end
