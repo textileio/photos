6ac7c90f9bb52f761d0fbf773f0cd292942deefc
Aaron Sutula <aaron@textile.io>
2018-03-15 16:22:42 -0600

    Clean up old Mobile.framework integration, add react-native-textile-ipfs npm module

---
+++ ios/Pods/Target Support Files/QBImagePickerController/QBImagePickerController-umbrella.h
@@ -0,0 +1,25 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "QBAlbumCell.h"
+#import "QBAlbumsViewController.h"
+#import "QBAssetCell.h"
+#import "QBAssetsViewController.h"
+#import "QBCheckmarkView.h"
+#import "QBImagePickerController.h"
+#import "QBSlomoIconView.h"
+#import "QBVideoIconView.h"
+#import "QBVideoIndicatorView.h"
+
+FOUNDATION_EXPORT double QBImagePickerControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char QBImagePickerControllerVersionString[];
+
--- ios/TextileGoModule.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <React/RCTBridgeModule.h>
-
-@interface TextileGoModule : NSObject <RCTBridgeModule>
-
-@end
