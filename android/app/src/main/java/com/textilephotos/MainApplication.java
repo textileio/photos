ffd599e39c983196c2cbe60af3a8aee5275cfba5
andrew <andrewxhill@gmail.com>
2018-03-28 19:46:58 -0700

    all methods stubbed out

--- android/app/src/main/java/com/textilephotos/MainApplication.java
+++ android/app/src/main/java/com/textilephotos/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.textilephotos.textileipfs.TextileIPFSPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,7 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
             new ImageResizerPackage(),
             new ReactNativeI18n(),
             new VectorIconsPackage(),
-            new RNDeviceInfo()
+            new RNDeviceInfo(),
+            new TextileIPFSPackage()
       );
     }
 
