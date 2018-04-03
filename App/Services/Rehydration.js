a856f3f5e2c939da3fbcec72997348ee069aaf25
Aaron Sutula <aaron@textile.io>
2018-04-03 16:48:56 -0500

    Refactor persistor creation to make it available to the PersistGate

--- App/Services/Rehydration.js
+++ App/Services/Rehydration.js
@@ -1,12 +1,10 @@
 import ReduxPersist from '../Config/ReduxPersist'
 import { AsyncStorage } from 'react-native'
-import { persistStore } from 'redux-persist'
-import StartupActions from '../Redux/StartupRedux'
 import DebugConfig from '../Config/DebugConfig'
+import {Persistor} from 'redux-persist/es/types'
 
-const updateReducers = (store: Object) => {
+const updateReducers = (store: Object, persistor: Persistor) => {
   const reducerVersion = ReduxPersist.reducerVersion
-  const startup = () => store.dispatch(StartupActions.startup())
 
   // Check to ensure latest reducer version
   AsyncStorage.getItem('reducerVersion').then((localVersion) => {
@@ -23,13 +21,10 @@ const updateReducers = (store: Object) => {
         })
       }
       // Purge store
-      persistStore(store, null, startup).purge()
+      persistor.purge()
       AsyncStorage.setItem('reducerVersion', reducerVersion)
-    } else {
-      persistStore(store, null, startup)
     }
   }).catch(() => {
-    persistStore(store, null, startup)
     AsyncStorage.setItem('reducerVersion', reducerVersion)
   })
 }
