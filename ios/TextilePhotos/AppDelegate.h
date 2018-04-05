b6c6b75449be88ea9f9618d6ed29b22ae5e0491b
Aaron Sutula <aaron@textile.io>
2018-04-04 20:37:39 -0500

    WIP, added objective c implementation of background upload task

--- ios/TextilePhotos/AppDelegate.h
+++ ios/TextilePhotos/AppDelegate.h
@@ -12,5 +12,6 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
+@property (nonatomic, copy, nullable) void (^backgroundCompletionHandler)();
 
 @end
