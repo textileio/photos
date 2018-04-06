e9ef6fc89eea762107cc2c69d04c4555010a0045
andrew <andrewxhill@gmail.com>
2018-04-05 18:27:43 -0700

    another round

--- App/Containers/Styles/TextilePhotosStyle.js
+++ App/Containers/Styles/TextilePhotosStyle.js
@@ -3,15 +3,43 @@ import { ApplicationStyles, Metrics, Colors } from '../../Themes'
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
-  container: {
+  navigationBar: {
     flex: 1,
-    borderTopColor: Colors.transparent,
-    borderTopWidth: 0,
-    borderBottomColor: Colors.test,
-    borderBottomWidth: 0,
-    paddingLeft: 18,
-    paddingRight: 18,
-    paddingTop: 20
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    backgroundColor: Colors.transparent,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+  navigationIcon: {
+    flex: 1,
+    // position: 'relative',
+    flexDirection: 'row',
+    color: Colors.frost,
+    backgroundColor: Colors.transparent,
+  },
+  container: {
+    // flex: 1,
+    // borderTopColor: Colors.transparent,
+    // borderTopWidth: 0,
+    // borderBottomColor: Colors.test,
+    // borderBottomWidth: 0,
+    // // paddingLeft: 0,
+    // // paddingRight: 0,
+    // paddingLeft: 18,
+    // paddingRight: 18,
+
+    flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: 20,
+    paddingLeft: 0,
+    paddingRight: 0,
+    backgroundColor: Colors.transparent,
   },
   backgroundImage: {
     position: 'absolute',
@@ -20,36 +48,50 @@ export default StyleSheet.create({
     left: 0,
     right: 0,
   },
-  row: {
+  cardStyle: {
+  },
+  statusCell: {
+  },
+  photoCell: {
+  },
+  photoStatus: {
     flex: 1,
-    backgroundColor: Colors.transparent,
-    // marginVertical: Metrics.smallMargin,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // margin: 2,
-    marginTop: 18,
-    marginBottom: 12,
+    flexDirection: 'row',
+    alignSelf: 'flex-end',
   },
-  boldLabel: {
-    fontWeight: 'bold',
+  noPhotos: {
     alignSelf: 'center',
-    color: Colors.ember,
-    textAlign: 'center',
-    marginBottom: Metrics.smallMargin
   },
-  label: {
-    textAlign: 'center',
-    color: Colors.snow
+  statusWhite: {
+    width: 10,
+    height: 10,
+    marginRight: 6,
+    borderWidth: 1.5,
+    borderColor: Colors.silver,
+    backgroundColor: Colors.transparent,
   },
-  listContent: {
-    marginTop: 0, ///Metrics.baseMargin
+  statusPink: {
+    width: 10,
+    height: 10,
+    marginRight: 6,
+    backgroundColor: Colors.brandPink,
   },
-  statusLabel: {
-    position: 'absolute',
-    textAlign: 'center',
-    backgroundColor: Colors.windowTint,
-    color: Colors.brandPink,
-    paddingVertical: 4,
-    paddingHorizontal: 6
+  statusRed: {
+    width: 10,
+    height: 10,
+    marginRight: 6,
+    backgroundColor: Colors.brandRed,
+  },
+  statusBlue: {
+    width: 10,
+    height: 10,
+    marginRight: 6,
+    backgroundColor: Colors.brandBlue,
+  },
+  message: {
+    flex: 1,
+    width: '90%',
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
   }
 })
