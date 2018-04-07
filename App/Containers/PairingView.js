25d4d2075fdeda6eb457945044c99b7bb5ea1c54
andrew <andrewxhill@gmail.com>
2018-04-07 14:44:28 -0700

    Updates deep link parsing to show confirmation code during pairing step

--- App/Containers/PairingView.js
+++ App/Containers/PairingView.js
@@ -18,13 +18,24 @@ class PairingView extends React.PureComponent {
   }
 
   getParams(url) {
-    var regex = /[?&]([^=#]+)=([^&#]*)/g,
-      params = {},
-      match;
-    while (match = regex.exec(url)) {
-      params[match[1]] = match[2];
+    let query = url.split('?')[1]
+    let vars = query.split("&")
+    let queryString = {};
+    for (let i = 0; i < vars.length; i++) {
+      let pair = vars[i].split("=");
+      // If first entry with this name
+      if (typeof queryString[pair[0]] === "undefined") {
+        queryString[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+      } else if (typeof queryString[pair[0]] === "string") {
+        let arr = [queryString[pair[0]], decodeURIComponent(pair[1])];
+        queryString[pair[0]] = arr;
+        // If third or later entry with this name
+      } else {
+        queryString[pair[0]].push(decodeURIComponent(pair[1]));
+      }
     }
-    return params
+    return queryString
   }
   render () {
     let code = 'WARN'
@@ -34,7 +45,6 @@ class PairingView extends React.PureComponent {
         code = params['code']
       }
     }
-    //{}
     return (
       <ImageBackground
         source={require('../Images/backgrounds/log-background.png')}
