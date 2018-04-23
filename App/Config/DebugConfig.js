cf3422aface34094f867d52e1e7acaececb601ac
andrew <andrewxhill@gmail.com>
2018-04-23 11:37:02 -0700

    Disables yellow warning boxes in simulator ui

--- App/Config/DebugConfig.js
+++ App/Config/DebugConfig.js
@@ -2,7 +2,7 @@ export default {
   showDevScreens: __DEV__,
   useFixtures: false,
   ezLogin: false,
-  yellowBox: __DEV__,
+  yellowBox: false,
   reduxLogging: __DEV__,
   includeExamples: __DEV__,
   useReactotron: __DEV__
