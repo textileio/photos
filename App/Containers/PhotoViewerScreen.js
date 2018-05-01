2e729e9675b41ef16460baa5100547d193f8b7a1
Carson Farmer <carson.farmer@gmail.com>
2018-05-01 12:08:05 -0700

    fix(revert): revert photoviewerscreen to 0a4211c58d075ecf9d5dad885ab335b553f57cc8

--- App/Containers/PhotoViewerScreen.js
+++ App/Containers/PhotoViewerScreen.js
@@ -1,37 +1,18 @@
 import React from 'react'
-import {
-  ScrollView,
-  Text,
-  KeyboardAvoidingView,
-  View,
-  StatusBar,
-  Image,
-  Linking
-} from 'react-native'
+import {ScrollView, Text, KeyboardAvoidingView, View, StatusBar, Image, Linking} from 'react-native'
 import Gallery from 'react-native-image-gallery'
 import { Icon } from 'react-native-elements'
 import Toast, {DURATION} from 'react-native-easy-toast'
 import { connect } from 'react-redux'
 import IPFS from '../../TextileIPFSNativeModule'
+// Add Actions - replace 'Your' with whatever your reducer is called :)
+// import YourActions from '../Redux/YourRedux'
 
 // Styles
 import styles from './Styles/PhotoViewerScreenStyle'
 import {buttonColor1} from "./Styles/InfoViewStyle";
 
 class PhotoViewerScreen extends React.PureComponent {
-
-  // componentWillMount() {
-  //   // TODO: Do something with this? Do we need the metadata?
-  //   const item = this.props.navigation.state.params.item
-  //   fetch('https://localhost:9080/ipfs/' + item.image.hash + '/meta')
-  //     .then((response) => response.json())
-  //     .then((responseJson) => {
-  //       this.setState({meta: new Date(responseJson.created)});
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     })
-  // }
 
   dismissPressed () {
     this.props.screenProps.dismiss()
@@ -41,7 +22,8 @@ class PhotoViewerScreen extends React.PureComponent {
     this.refs.toast.show('Sharing coming soon!', DURATION.LENGTH_SHORT)
   }
 
-  get viewScreen () {
+  get galleryCount () {
+    // const { index, images } = this.state;
     return (
       <View style={{ flex: 1, flexDirection: 'row', padding: 8, top: 0, height: 60, backgroundColor: 'rgba(0, 0, 0, 0.7)', width: '100%', position: 'absolute', justifyContent: 'space-between' }}>
         <Icon name='close' type='evilicon' color='#FFFFFF' underlayColor='rgba(0, 0, 0, 0)' size={44} onPress={this.dismissPressed.bind(this)} />
@@ -50,33 +32,38 @@ class PhotoViewerScreen extends React.PureComponent {
     )
   }
 
-  // get caption () {
-  //   return (
-  //     <View style={{ bottom: 0, height: 65, backgroundColor: 'rgba(0, 0, 0, 0.7)', width: '100%', position: 'absolute', justifyContent: 'center' }}>
-  //       <Text style={{ textAlign: 'center', color: 'white', fontSize: 15, fontStyle: 'italic' }}>{'Some metadata'}</Text>
-  //     </View>
-  //   )
-  // }
-
-  renderImage(image) {
+  get caption () {
     return (
-      <View style={{ flex: 1, flexDirection: 'row', backgroundColor: 'rgba(0, 0, 0, 1)' }}>
-        <Image
-          source={{uri: 'https://localhost:9080/ipfs/' + image.hash + '/photo'}}
-          style={{ flex: 1, width: null, height: null }}
-          resizeMode="contain"
-        />
+      <View style={{ bottom: 0, height: 65, backgroundColor: 'rgba(0, 0, 0, 0.7)', width: '100%', position: 'absolute', justifyContent: 'center' }}>
+        <Text style={{ textAlign: 'center', color: 'white', fontSize: 15, fontStyle: 'italic' }}>{'Some metadata'}</Text>
       </View>
     )
   }
 
+  renderImage(image) {
+    const imageData = IPFS.syncGetPhotoData(image.image.hash + '/thumb')
+    return (
+      <Image
+        source={{uri: 'data:image/jpeg;base64,' + imageData}}
+        style={image.style}
+        resizeMode={image.resizeMode}
+        capInsets={image.capInsets}
+      />
+    )
+  }
+
   render () {
-    const item = this.props.navigation.state.params.item
     return (
       <View style={{flex: 1}}>
         <StatusBar hidden />
-        { this.renderImage(item.image) }
-        { this.viewScreen }
+        <Gallery
+          style={{ flex: 1, backgroundColor: 'black' }}
+          imageComponent={this.renderImage}
+          images={this.props.imageData}
+          initialPage={this.props.navigation.state.params.index}
+        />
+        { this.galleryCount }
+        { this.caption }
         <Toast ref='toast' position='center' />
       </View>
     )
@@ -84,7 +71,16 @@ class PhotoViewerScreen extends React.PureComponent {
 }
 
 const mapStateToProps = (state) => {
+  const hashes = state.ipfs.photos.hashes
+  const imageData = hashes.map(hash => {
+    return {
+      source: { uri: 'file:///image.jpg' },
+      hash,
+      dimensions: { width: 100, height: 100 }
+    }
+  })
   return {
+    imageData
   }
 }
 
