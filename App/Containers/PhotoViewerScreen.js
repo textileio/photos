78f799f95afcde816fbe423bab0d06de88e7175b
Carson Farmer <carson.farmer@gmail.com>
2018-05-01 11:32:29 -0700

    fix(gateway): some simple cleanups

--- App/Containers/PhotoViewerScreen.js
+++ App/Containers/PhotoViewerScreen.js
@@ -20,18 +20,18 @@ import {buttonColor1} from "./Styles/InfoViewStyle";
 
 class PhotoViewerScreen extends React.PureComponent {
 
-  componentWillMount() {
-    // TODO: Do something with this? Do we need the metadata?
-    const item = this.props.navigation.state.params.item
-    fetch('https://localhost:9080/ipfs/' + item.image.hash + '/meta')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({meta: new Date(responseJson.created)});
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-  }
+  // componentWillMount() {
+  //   // TODO: Do something with this? Do we need the metadata?
+  //   const item = this.props.navigation.state.params.item
+  //   fetch('https://localhost:9080/ipfs/' + item.image.hash + '/meta')
+  //     .then((response) => response.json())
+  //     .then((responseJson) => {
+  //       this.setState({meta: new Date(responseJson.created)});
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     })
+  // }
 
   dismissPressed () {
     this.props.screenProps.dismiss()
@@ -41,8 +41,7 @@ class PhotoViewerScreen extends React.PureComponent {
     this.refs.toast.show('Sharing coming soon!', DURATION.LENGTH_SHORT)
   }
 
-  get galleryCount () {
-    // const { index, images } = this.state;
+  get viewScreen () {
     return (
       <View style={{ flex: 1, flexDirection: 'row', padding: 8, top: 0, height: 60, backgroundColor: 'rgba(0, 0, 0, 0.7)', width: '100%', position: 'absolute', justifyContent: 'space-between' }}>
         <Icon name='close' type='evilicon' color='#FFFFFF' underlayColor='rgba(0, 0, 0, 0)' size={44} onPress={this.dismissPressed.bind(this)} />
@@ -51,13 +50,13 @@ class PhotoViewerScreen extends React.PureComponent {
     )
   }
 
-  get caption () {
-    return (
-      <View style={{ bottom: 0, height: 65, backgroundColor: 'rgba(0, 0, 0, 0.7)', width: '100%', position: 'absolute', justifyContent: 'center' }}>
-        <Text style={{ textAlign: 'center', color: 'white', fontSize: 15, fontStyle: 'italic' }}>{'Some metadata'}</Text>
-      </View>
-    )
-  }
+  // get caption () {
+  //   return (
+  //     <View style={{ bottom: 0, height: 65, backgroundColor: 'rgba(0, 0, 0, 0.7)', width: '100%', position: 'absolute', justifyContent: 'center' }}>
+  //       <Text style={{ textAlign: 'center', color: 'white', fontSize: 15, fontStyle: 'italic' }}>{'Some metadata'}</Text>
+  //     </View>
+  //   )
+  // }
 
   renderImage(image) {
     return (
@@ -77,8 +76,7 @@ class PhotoViewerScreen extends React.PureComponent {
       <View style={{flex: 1}}>
         <StatusBar hidden />
         { this.renderImage(item.image) }
-        { this.galleryCount }
-        { this.caption }
+        { this.viewScreen }
         <Toast ref='toast' position='center' />
       </View>
     )
