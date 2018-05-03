d34055e5c69c1c334f3312fb325e55d421bde287
Aaron Sutula <aaron@textile.io>
2018-05-02 19:11:43 -0500

    WIP on sharing dialog

---
+++ App/Containers/SharingDialog.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { ScrollView, Text } from 'react-native'
+import { connect } from 'react-redux'
+// Add Actions - replace 'Your' with whatever your reducer is called :)
+// import YourActions from '../Redux/YourRedux'
+
+// Styles
+import styles from './Styles/SharingDialogStyle'
+
+class SharingDialog extends Component {
+  // constructor (props) {
+  //   super(props)
+  //   this.state = {}
+  // }
+
+  render () {
+    return (
+      <ScrollView style={styles.container}>
+        <Text>SharingDialog Container</Text>
+      </ScrollView>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SharingDialog)
