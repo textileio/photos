42378a51fca5afda1564641d4c28870f1d14dae2
Carson Farmer <carson.farmer@gmail.com>
2018-05-03 14:44:37 -0700

    update where cookie is set

--- App/Containers/TextileManager.js
+++ App/Containers/TextileManager.js
@@ -10,8 +10,6 @@ import BackgroundTask from 'react-native-background-task'
 import TextileActions from '../Redux/TextileRedux'
 import PhotosNavigation from '../Navigation/PhotosNavigation'
 import Upload from 'react-native-background-upload'
-import CookieManager from 'react-native-cookies'
-import IPFS from '../../TextileIPFSNativeModule'
 
 class TextileManager extends React.PureComponent {
   constructor () {
@@ -39,27 +37,6 @@ class TextileManager extends React.PureComponent {
       })
     } else {
       Linking.addEventListener('url', this._handleOpenURLEvent.bind(this))
-    }
-
-    const value = yield call(IPFS.getGatewayPassword)
-
-    if (Platform.OS === 'android') {
-      CookieManager.setFromResponse(
-        'https://localhost:9080',
-        'SessionId=Password; path=/; expires=Thu, 1 Jan 2030 00:00:00 -0000; secure; HttpOnly')
-          .then((res) => {
-            console.log('CookieManager.setFromResponse =>', res);
-          });
-    } else {
-      CookieManager.set({
-        name: 'SessionId',
-        value: 'Password',
-        origin: 'https://localhost:9080',
-        domain: '.localhost',
-        version: '1',
-        path: '/',
-        expiration: '2030-01-01T00:00:00.00-00:00'
-      })
     }
   }
 
