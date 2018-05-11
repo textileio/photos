a06c82cd8f9c929a74f8333700106fc16030a7c7
andrew <andrewxhill@gmail.com>
2018-05-11 15:34:01 -0700

    changes android package name. updates default text on empty threads

--- App/Containers/TextilePhotos.js
+++ App/Containers/TextilePhotos.js
@@ -24,6 +24,7 @@ class TextilePhotos extends React.PureComponent {
         onSelect={this.onSelect}
         onRefresh={this.onRefresh.bind(this)}
         refreshing={this.props.refreshing}
+        placeholderText={this.props.placeholderText}
       />
     )
   }
@@ -40,11 +41,15 @@ const mapStateToProps = (state, ownProps) => {
     }
   }
   const updatedItems = Object.values(allItemsObj).sort((a, b) => a.index > b.index)
+  const placeholderText = thread === 'default'
+    ? 'Any new photos you take will be added to your Textile wallet.'
+    : 'Share your first photo to the Beta Testers Thread.'
   return {
     thread,
     items: updatedItems,
     renderImages: state.ipfs.nodeState.state === 'started',
-    refreshing: state.ipfs.threads[thread].querying
+    refreshing: state.ipfs.threads[thread].querying,
+    placeholderText
   }
 }
 
