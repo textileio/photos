1ac0b32a43056a05a386c8234d6a97e340eaefd8
andrew <andrewxhill@gmail.com>
2018-05-11 15:54:42 -0700

    adds missing files!

---
+++ android/app/src/main/java/com/textile/textileipfs/TextileIPFSPackage.java
@@ -0,0 +1,33 @@
+//  Created by react-native-create-bridge
+
+package com.textile.textileipfs;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TextileIPFSPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+      // Register your native module
+      // https://facebook.github.io/react-native/docs/native-modules-android.html#register-the-module
+//      return Arrays.<NativeModule>asList(
+//          new TextileIPFSModule(reactContext)
+//      );
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new TextileIPFSModule(reactContext));
+
+        return modules;
+    }
+
+    
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
