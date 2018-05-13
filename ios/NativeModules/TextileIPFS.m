ede13ab7470475438afe28a0058e9db20a9edf27
Sander Pick <sanderpick@gmail.com>
2018-05-13 14:58:32 -0700

    make messenger event generic, include android bridge update

--- ios/NativeModules/TextileIPFS.m
+++ ios/NativeModules/TextileIPFS.m
@@ -23,8 +23,8 @@
 
 @implementation Messenger
 
-- (void) notify: (CoreThreadUpdate *)update {
-  [Events emitEventWithName:@"onThreadUpdate" andPayload:@{@"cid":update.cid, @"thread":update.thread, @"thread_id":update.threadID}];
+- (void) notify: (MobileEvent *)event {
+  [Events emitEventWithName:event.name andPayload:event.payload];
 }
 
 @end
