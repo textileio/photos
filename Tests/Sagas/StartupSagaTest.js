dd29320c1e922731143ba6baaaf661f7a1136e09
Carson Farmer <carson.farmer@gmail.com>
2018-05-24 16:10:04 -0700

    remove default tests

--- Tests/Sagas/StartupSagaTest.js
+++
@@ -1,8 +0,0 @@
-import { select, put } from 'redux-saga/effects'
-import { selectAvatar, startup } from '../../App/Sagas/StartupSagas'
-
-const stepper = (fn) => (mock) => fn.next(mock).value
-
-test('watches for the right action', () => {
-  const step = stepper(startup())
-})
