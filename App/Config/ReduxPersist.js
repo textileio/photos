2b0b8aa3b050e2b8f7218fe6b07916dbb0bad2ca
Aaron Sutula <aaron@textile.io>
2018-05-31 19:38:45 -0500

    Migration to add user preferences

--- App/Config/ReduxPersist.js
+++ App/Config/ReduxPersist.js
@@ -1,5 +1,21 @@
 import immutablePersistenceTransform from '../Services/ImmutablePersistenceTransform'
 import { AsyncStorage } from 'react-native'
+import { createMigrate } from 'redux-persist'
+
+const migrations = {
+  0: (state) => {
+    // migration clear out device state
+    return {
+      ...state,
+      textile: {
+        ...state.textile,
+        preferences: {
+          verboseUi: false
+        }
+      }
+    }
+  }
+}
 
 // More info here:  https://shift.infinite.red/shipping-persistant-reducers-7341691232b1
 const REDUX_PERSIST = {
@@ -7,7 +23,9 @@ const REDUX_PERSIST = {
   reducerVersion: '1.0',
   storeConfig: {
     key: 'primary',
+    version: 0,
     storage: AsyncStorage,
+    migrate: createMigrate(migrations, { debug: false }),
     // Reducer keys that you do NOT want stored to persistence here.
     blacklist: ['nav', 'ipfs', 'auth', 'ui'],
     // Optionally, just specify the keys you DO want stored to persistence.
