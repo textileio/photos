80e25441322226dc7c37ddaf3d7e2ae69d7db1f2
Sander Pick <sanderpick@gmail.com>
2018-06-04 23:28:40 -0700

    frameworks for texgo-135, adds onOnline event

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -17,8 +17,11 @@ BackgroundTask.define(() => {
   store.dispatch(Actions.backgroundTask())
 })
 
-// subscribe to thread updates
+// subscribe to native events
 // NOTE: we may want to cancel listener with the returned handle at some point with subscription.remove()
+IPFS.eventEmitter.addListener('onOnline', () => {
+  // TODO: show some UI? "p2p online"?
+})
 IPFS.eventEmitter.addListener('onThreadUpdate', (payload) => {
   store.dispatch(IPFSActions.getPhotoHashesRequest(payload.thread))
 })
