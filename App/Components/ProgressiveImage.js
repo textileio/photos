b7066faeb3dd79eb401d77977749f4ae56dc1bf3
andrew <andrewxhill@gmail.com>
2018-06-08 16:05:42 -0700

    go ahead and remove thumbs from render stack when full is loaded

--- App/Components/ProgressiveImage.js
+++ App/Components/ProgressiveImage.js
@@ -7,8 +7,14 @@ export default class ProgressiveImage extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      loaded: false
+      loaded: false,
+      preview: false
     }
+  }
+
+  _onPreview () {
+    // just drop the thumb/default from the stack
+    this.setState(() => ({preview: true}))
   }
 
   _onLoaded () {
@@ -16,12 +22,13 @@ export default class ProgressiveImage extends React.Component {
     this.setState(() => ({loaded: true}))
   }
 
-  renderPreview () {
+  get renderPreview () {
     return (<TextileImage
       source={this.props.previewSource}
       style={[{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0}, this.props.style]}
       resizeMode={this.props.resizeMode}
       capInsets={this.props.capInsets}
+      onLoad={this._onPreview.bind(this)}
     />)
   }
 
@@ -37,11 +44,28 @@ export default class ProgressiveImage extends React.Component {
   }
 
   render () {
-    return (
-      <View style={this.props.style}>
-        {this.renderPreview()}
-        {this.renderPhoto()}
-      </View>
-    )
+    if (this.state.loaded) {
+      // if full res is available, don't bother showing thumb in stack
+      return (
+        <View style={this.props.style}>
+          {this.renderPhoto()}
+        </View>
+      )
+    } else if (this.state.preview) {
+      // if thumb is available, start loading the full res
+      return (
+        <View style={this.props.style}>
+          {this.renderPreview}
+          {this.renderPhoto()}
+        </View>
+      )
+    } else {
+      // default, begin loading the thumb
+      return (
+        <View style={this.props.style}>
+          {this.renderPreview}
+        </View>
+      )
+    }
   }
 }
