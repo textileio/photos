86052158d5cbaf8d63a04c34330738bb3905f5c2
Aaron Sutula <aaron@textile.io>
2018-07-18 13:44:00 -0500

    Playing around with photo querying

--- App/Containers/TextileManager.js
+++ App/Containers/TextileManager.js
@@ -13,6 +13,7 @@ import PhotosNavigation from '../Navigation/PhotosNavigation'
 import Upload from 'react-native-background-upload'
 import PhotosNavigationService from '../Services/PhotosNavigationService'
 import DeepLink from '../Services/DeepLink'
+import { foo } from '../Services/CameraRoll'
 
 class TextileManager extends React.PureComponent {
   componentDidMount () {
@@ -26,6 +27,8 @@ class TextileManager extends React.PureComponent {
     } else {
       Linking.addEventListener('url', this._handleOpenURLEvent.bind(this))
     }
+
+    foo()
   }
 
   _handleOpenURLEvent (event) {
