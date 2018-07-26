50299f919123613e46efd89f7783a1eac54b7b21
andrew <andrewxhill@gmail.com>
2018-07-26 15:46:32 -0700

    adds thread view for photo

--- App/SB/components/CommentCard/index.js
+++ App/SB/components/CommentCard/index.js
@@ -3,14 +3,35 @@ import { View, Image, Text } from 'react-native'
 
 import SmallIconTag from '../SmallIconTag'
 
+import moment from 'moment'
+import SvgUri from 'react-native-svg-uri'
+const jdenticon = require('jdenticon')
+
 import styles from './statics/styles'
 
 const CommentCard = props => {
-  const { userName, comment, date, subComments, isSubComment, photo } = props
+  // const { userName, comment, date, subComments, isSubComment, photo } = props
+  const userName = props.metadata.username
+  const comment = props.photo.caption
+  const date = moment.utc(props.metadata.added).fromNow()
+  const subComments = undefined
+  const isSubComment = false
+
+  let avatar = ''
+  try {
+    avatar = jdenticon.toSvg(userName, 38)
+  } catch (err) {}
 
   return (
     <View style={[styles.comment, isSubComment ? styles.subComment : styles.withDivider ]}>
-      <Image style={styles.commentImage} source={photo} />
+      {/*<Image style={styles.commentImage} source={photo} />*/}
+      <View style={styles.commentImage}>
+        <SvgUri
+          width={styles.commentImage.width}
+          height={styles.commentImage.height}
+          svgXmlData={avatar}
+        />
+      </View>
       <View style={styles.commentTexts}>
         <Text style={styles.commentUser}>{userName}</Text>
         <Text style={styles.commentText}>{comment}</Text>
