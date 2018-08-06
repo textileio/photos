dae37c4ce4c63d5b752caefce3b4bcaa493c7a70
Aaron Sutula <aaron@textile.io>
2018-08-06 17:31:58 -0500

    Android Crashlytics install

--- android/app/src/main/java/com/textile/MainActivity.java
+++ android/app/src/main/java/com/textile/MainActivity.java
@@ -1,6 +1,11 @@
 package com.textile;
 
+import android.os.Bundle;
+
+import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactActivity;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +16,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "TextilePhotos";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
     }
 }
