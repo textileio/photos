464e7a4d99fa272237d788922a3122ea09b3c820
Aaron Sutula <aaron@textile.io>
2018-08-07 14:31:06 -0500

    Action type update for consistency

--- App/Redux/CameraRollRedux.ts
+++ App/Redux/CameraRollRedux.ts
@@ -31,7 +31,7 @@ const actions = {
   localPinSuccess: createAction('LOCAL_PIN_SUCCESS', resolve => {
     return (threadId: string, image: TextileTypes.SharedImage, addResult: TextileTypes.AddResult) => resolve({ threadId, image, addResult })
   }),
-  remotePinStarted: createAction('REMOTE_PIN_SUCCESS', resolve => {
+  remotePinStarted: createAction('REMOTE_PIN_STARTED', resolve => {
     return (threadId: string, image: TextileTypes.SharedImage) => resolve({ threadId, image })
   }),
   imagePinError: createAction('IMAGE_PICKER_PIN_ERROR', resolve => {
