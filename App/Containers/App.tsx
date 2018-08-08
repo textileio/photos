67c2ad99993d0f17990ebd028e4006f5f23de8e4
andrew <andrewxhill@gmail.com>
2018-08-08 14:42:36 -0700

    first pass

--- App/Containers/App.tsx
+++ App/Containers/App.tsx
@@ -6,6 +6,7 @@ import {PersistGate} from 'redux-persist/integration/react'
 import RootContainer from './RootContainer'
 import configureStore from '../Redux/configureStore'
 import TriggersActions from '../Redux/TriggersRedux'
+import NotificationActions from '../Redux/NotificationsRedux'
 import TextileNodeActions from '../Redux/TextileNodeRedux'
 import ThreadActions from '../Redux/ThreadsRedux'
 import BackgroundTask from 'react-native-background-task'
@@ -21,22 +22,50 @@ BackgroundTask.define(() => {
 // NOTE: we may want to cancel listener with the returned handle at some point with subscription.remove()
 TextileNode.eventEmitter.addListener('onOnline', () => {
   store.dispatch(TextileNodeActions.nodeOnline())
+  store.dispatch(NotificationActions.newNotification({
+    category: 'node',
+    type: 'onOnline',
+    date: Date.now()
+  }))
 })
 // TODO: add types to event emitter if possible
 TextileNode.eventEmitter.addListener('onThreadUpdate', (payload) => {
   store.dispatch(TextileNodeActions.getPhotoHashesRequest(payload.thread_id))
+  store.dispatch(NotificationActions.newNotification({
+    category: 'content',
+    type: 'onThreadUpdate',
+    date: Date.now(),
+    payload: payload
+  }))
 })
 TextileNode.eventEmitter.addListener('onThreadAdded', () => {
   store.dispatch(ThreadActions.refreshThreadsRequest())
+  store.dispatch(NotificationActions.newNotification({
+    category: 'threads',
+    type: 'onThreadAdded',
+    date: Date.now()
+  }))
 })
 TextileNode.eventEmitter.addListener('onThreadRemoved', () => {
   store.dispatch(ThreadActions.refreshThreadsRequest())
+  store.dispatch(NotificationActions.newNotification({
+    category: 'threads',
+    type: 'onThreadRemoved',
+    date: Date.now()
+  }))
 })
 TextileNode.eventEmitter.addListener('onDeviceAdded', () => {
-  //
+  store.dispatch(NotificationActions.newNotification({
+    category: 'devices',
+    type: 'onDeviceAdded',
+    date: Date.now()
+  }))
 })
 TextileNode.eventEmitter.addListener('onDeviceRemoved', () => {
-  //
+  store.dispatch(NotificationActions.newNotification({
+    category: 'devices',
+    type: 'onDeviceRemoved'
+  }))
 })
 class App extends Component {
   render () {
--- App/Redux/Types.ts
+++ App/Redux/Types.ts
@@ -7,20 +7,21 @@ import { CameraRollAction } from './CameraRollRedux'
 import { ContactsAction } from './ContactsRedux'
 import { DevicesAction } from './DevicesRedux'
 import { PreferencesAction } from './PreferencesRedux'
+import { NotificationsAction } from './NotificationsRedux'
 import { TextileNodeAction } from './TextileNodeRedux'
 import { ThreadsAction } from './ThreadsRedux'
 import { UIAction } from './UIRedux'
 import { UploadingImagesAction } from './UploadingImagesRedux'
 
 export type RootState = StateType<typeof RootReducer> & PersistPartial
-export type RootAction = 
+export type RootAction =
   AuthAction |
   CameraRollAction |
   ContactsAction |
   DevicesAction |
   PreferencesAction |
+  NotificationsAction |
   TextileNodeAction |
   ThreadsAction |
   UIAction |
   UploadingImagesAction
-  
