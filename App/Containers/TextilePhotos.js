3fc5813ec04b60c9d8e97d2ee3df735f94c84009
andrew <andrewxhill@gmail.com>
2018-08-09 11:18:43 -0700

    isolating all the avatars into one component

--- App/Containers/TextilePhotos.js
+++ App/Containers/TextilePhotos.js
@@ -9,6 +9,7 @@ import UIActions from '../Redux/UIRedux'
 import ThreadsActions from '../Redux/ThreadsRedux'
 import style from './Styles/TextilePhotosStyle'
 import navStyles from '../Navigation/Styles/NavigationStyles'
+import Avatar from '../Components/Avatar'
 
 import BottomDrawerList from '../SB/components/BottomDrawerList'
 import NavigationService from '../Services/NavigationService'
@@ -23,23 +24,15 @@ class TextilePhotos extends React.PureComponent {
     const avatarUrl = params.profile && params.profile.avatar_id ? 'https://cafe.us-east-1.textile.io' + params.profile.avatar_id : undefined
     const username = params.profile && params.profile.username ? params.profile.username : undefined
     const headerLeft = (
-      <TouchableWithoutFeedback
+      <TouchableOpacity
         delayLongPress={3000}
         onLongPress={params.toggleVerboseUi}
         onPress={() => {
           navigation.navigate('Account', {avatarUrl, username})
         }}
       >
-        <View style={navStyles.headerIconUser}>
-          <View style={navStyles.iconContainer}>
-            {(avatarUrl) && <Image
-              source={{uri: avatarUrl}}
-              resizeMode={'cover'}
-              style={{width: 24, height: 24}}
-            />}
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
+        <Avatar width={24} height={24} uri={avatarUrl} defaultSource={require('../SB/views/Settings/statics/main-image.png')} />
+      </TouchableOpacity>
     )
     const headerRight = undefined
       // Wallet menu not available yet
