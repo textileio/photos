f6537bad195e89cf2caa2551a673d3bff3907057
andrew <andrewxhill@gmail.com>
2018-08-08 18:13:52 -0700

    cleans up default image

--- App/SB/components/CommentCard/index.js
+++ App/SB/components/CommentCard/index.js
@@ -12,40 +12,26 @@ import navStyles from '../../../Navigation/Styles/NavigationStyles'
 
 const CommentCard = props => {
   // const { userName, comment, date, subComments, isSubComment, photo } = props
-  const userName = props.metadata && props.metadata.username ? props.metadata.username : "anonymous"
+  const userName = props.metadata && props.metadata.username ? props.metadata.username : 'anonymous'
   const comment = props.photo.caption
   const date = moment.utc(props.photo.date).fromNow()
   const subComments = undefined
   const isSubComment = false
 
-  const profile = props.profiles.find((p) => p.id === props.photo.author_id)
-
-  let avatar = ''
-  if (!profile) {
-    try {
-      avatar = jdenticon.toSvg(userName, 38)
-    } catch (err) {}
-  }
+  const defaultSource = require('../../views/Settings/statics/main-image.png')
+  const src = props.metadata.peer_id ? {uri: 'https://cafe.us-east-1.textile.io/ipns/' + props.metadata.peer_id + '/avatar_id'} : defaultSource
 
   return (
     <View style={[styles.comment, isSubComment ? styles.subComment : styles.withDivider ]}>
       <View style={styles.commentProfileImage}>
-        {(profile && profile.avatar_id) &&
-          <View style={styles.profileContainer}>
-            <Image
-              source={{uri: profile.avatar_id}}
-              resizeMode={'cover'}
-              style={styles.commentImage}
-            />
-          </View>}
-        {(!profile || !profile.avatar_id) &&
-          <View style={styles.profileContainer}>
-            <SvgUri
-              width={styles.commentImage.width}
-              height={styles.commentImage.height}
-              svgXmlData={avatar}
-            />
-          </View>}
+        <View style={styles.profileContainer}>
+          <Image
+            source={src}
+            resizeMode={'cover'}
+            style={styles.commentImage}
+            defaultSource={defaultSource}
+          />
+        </View>
       </View>
       <View style={styles.commentTexts}>
         <Text style={styles.commentUser}>{userName}</Text>
