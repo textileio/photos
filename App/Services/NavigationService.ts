b173dc5a08f56296d9f9b7d0a724de4f3a4b9389
Aaron Sutula <aaron.sutula@gmail.com>
2018-08-09 16:32:55 -0500

    Remove TextileManager, consoldate NavigationService

--- App/Services/NavigationService.ts
+++ App/Services/NavigationService.ts
@@ -6,7 +6,7 @@ function setTopLevelNavigator (navigatorRef: NavigationContainerComponent) {
   _navigator = navigatorRef
 }
 
-function navigate (routeName: string, params: NavigationParams) {
+function navigate (routeName: string, params?: NavigationParams) {
   _navigator.dispatch(
     NavigationActions.navigate({
       // type: NavigationActions.NAVIGATE,
@@ -16,9 +16,16 @@ function navigate (routeName: string, params: NavigationParams) {
   )
 }
 
+function goBack () {
+  _navigator.dispatch(
+    NavigationActions.back()
+  )
+}
+
 // add other navigation functions that you need and export them
 
 export default {
   navigate,
+  goBack,
   setTopLevelNavigator,
 }
