db8a1f3557d66993e24ca860ce37a8679c73dfa1
andrew <andrewxhill@gmail.com>
2018-08-13 17:19:36 -0700

    combines methods

--- App/Sagas/TextileSagas.ts
+++ App/Sagas/TextileSagas.ts
@@ -104,6 +104,7 @@ export function * handleProfilePhotoUpdated(action: ActionType<typeof UIActions.
   let defaultThread: TextileTypes.Thread = yield call(getDefaultThread)
 
   yield processAvatarImage(action.payload.uri, defaultThread)
+  yield put(UIActions)
 }
 
 function * processAvatarImage(uri: string, defaultThread: TextileTypes.Thread) {
