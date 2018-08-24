690ee386c0160a5de4704823f6b5c8455c52bb37
Aaron Sutula <aaron.sutula@gmail.com>
2018-08-24 12:20:03 -0500

    Wire up tasks to node lifecycle events

---
+++ App/Sagas/NodeCreated.ts
@@ -0,0 +1,17 @@
+import { call, put, take } from 'redux-saga/effects'
+import { getType } from 'typesafe-actions'
+import TextileNode from '../../TextileNode'
+import PreferencesActions from '../Redux/PreferencesRedux'
+import TextileNodeActions from '../Redux/TextileNodeRedux'
+
+
+export function * onNodeCreated () {
+  while (yield take(getType(TextileNodeActions.createNodeSuccess))) {
+    try {
+      const mnemonic: string = yield call(TextileNode.mnemonic)
+      yield put(PreferencesActions.updatecMnemonic(mnemonic))
+    } catch {
+      // This only succeeds when the node is first created so this error is expected
+    }
+  }
+}
