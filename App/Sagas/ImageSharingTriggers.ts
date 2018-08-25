4bdae8697cd2d4b09aa6870b3aa65df1e2650ff8
andrew <andrewxhill@gmail.com>
2018-08-25 11:52:51 -0700

    cleanup a few types

--- App/Sagas/ImageSharingTriggers.ts
+++ App/Sagas/ImageSharingTriggers.ts
@@ -1,19 +1,20 @@
 import { call, put, select } from 'redux-saga/effects'
 import { ActionType } from 'typesafe-actions'
 import RNFS from 'react-native-fs'
+import { PhotoId } from '../Models/TextileTypes'
 
 import ProcessingImagesActions, { ProcessingImage, ProcessingImagesSelectors } from '../Redux/ProcessingImagesRedux'
 import UIActions from '../Redux/UIRedux'
 import { insertImage, addToIpfs, uploadArchive, shareWalletImage, addToWallet, shareToThread } from './ImageSharingSagas'
 
 export function * handleSharePhotoRequest (action: ActionType<typeof UIActions.sharePhotoRequest>) {
-  const { image, threadId, comment } = action.payload
-  if (!image || !threadId) {
+  const { image, imageId, threadId, comment } = action.payload
+  if ((!image && !imageId) || !threadId) {
     return
   }
-  if (typeof image === 'string') {
-    yield call(shareWalletImage, image, threadId, comment)
-  } else {
+  if (imageId) {
+    yield call(shareWalletImage, imageId, threadId, comment)
+  } else if (image) {
     yield call(insertImage, image, threadId, comment)
   }
 }
