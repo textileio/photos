6d4f257c1f70c55c2dfd2c2f9e6332a735217459
andrew <andrewxhill@gmail.com>
2018-09-05 18:20:13 -0400

    unlink svg lib

--- android/app/src/main/java/com/textile/MainApplication.java
+++ android/app/src/main/java/com/textile/MainApplication.java
@@ -8,7 +8,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.horcrux.svg.SvgPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.imagepicker.ImagePickerPackage;
 import com.jamesisaac.rnbackgroundtask.BackgroundTaskPackage;
