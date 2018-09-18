da239b51090257a12f08daeda0cba41473874d71
Aaron Sutula <aaron.sutula@gmail.com>
2018-09-17 20:01:56 -0500

    Move thread/comment releated screen to PrimaryNavigation

--- App/Navigation/AppNavigation/Primary/TabNavigator/Threads/index.ts
+++ App/Navigation/AppNavigation/Primary/TabNavigator/Threads/index.ts
@@ -2,26 +2,15 @@ import { createStackNavigator } from 'react-navigation'
 
 import ThreadsList from '../../../../../Containers/ThreadsList'
 import AddThreadScreen from '../../../../../Containers/AddThreadScreen'
-import ThreadDetail from '../../../../../SB/views/ThreadDetail'
-import ThreadsEditFriends from '../../../../../SB/views/ThreadsEditFriends'
-import Comments from '../../../../../Containers/Comments'
-import AddCaptionScreen from '../../../../../Containers/AddCaptionScreen'
-import WalletPicker from '../../../../../Containers/WalletPicker'
 
 import styles, { headerTintColor } from '../../../../Styles/NavigationStyles'
 
 const nav = createStackNavigator(
   {
     SharedPhotos: ThreadsList,
-    AddThread: AddThreadScreen,
-    AddFriends: ThreadsEditFriends,
-    ViewThread: ThreadDetail,
-    Comments,
-    ThreadSharePhoto: AddCaptionScreen,
-    WalletPicker
+    AddThread: AddThreadScreen
   },
   {
-    headerMode: 'float',
     navigationOptions: {
       headerStyle: styles.header,
       headerTitleStyle: styles.headerTitle,
--- App/Navigation/AppNavigation/Primary/index.ts
+++ App/Navigation/AppNavigation/Primary/index.ts
@@ -1,22 +1,36 @@
 import { createStackNavigator } from 'react-navigation'
 
 import TabNavigator from './TabNavigator'
+
 import Account from '../../../SB/views/UserProfile'
 import NotificationSettings from '../../../SB/views/Notifications'
 import Storage from '../../../SB/views/Storage'
 import Mnemonic from '../../../SB/views/UserProfile/Mnemonic'
 import UpdateAvatar from '../../../SB/views/UserOnBoarding/UpdateAvatar'
 
+import ThreadDetail from '../../../SB/views/ThreadDetail'
+import ThreadsEditFriends from '../../../SB/views/ThreadsEditFriends'
+import Comments from '../../../Containers/Comments'
+import AddCaptionScreen from '../../../Containers/AddCaptionScreen'
+import WalletPicker from '../../../Containers/WalletPicker'
+
 import styles, { headerTintColor } from '../../Styles/NavigationStyles'
 
 const nav = createStackNavigator(
   {
     TabNavigator,
+
     Account,
     NotificationSettings,
     Storage,
     Mnemonic,
-    ChangeAvatar: UpdateAvatar
+    ChangeAvatar: UpdateAvatar,
+
+    AddFriends: ThreadsEditFriends,
+    ViewThread: ThreadDetail,
+    Comments,
+    ThreadSharePhoto: AddCaptionScreen,
+    WalletPicker
   },
   {
     navigationOptions: {
