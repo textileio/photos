ec12cc90fcb6a33b5fc126902b38921d2d39777d
Aaron Sutula <aaron.sutula@gmail.com>
2018-10-04 16:58:20 -0500

    oops fix setup.js dup and lint

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,5 @@
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-global.fetch = require("jest-fetch-mock");
-Enzyme.configure({ adapter: new Adapter() });
+global.fetch = require('jest-fetch-mock')
+Enzyme.configure({ adapter: new Adapter() })
