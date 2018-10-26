7d03ab239d9bef10c005565dd14a6cfb7f003879
andrew <andrewxhill@gmail.com>
2018-10-26 10:56:57 -0700

    lint

--- App/Components/Styles/ThreadCreateModal.tsx
+++ App/Components/Styles/ThreadCreateModal.tsx
@@ -67,7 +67,7 @@ export default StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
-    paddingTop: 45,
+    paddingTop: 45
   } as ViewStyle,
   buttonText: {
     fontFamily: 'BentonSans',
