988a30a67460c67fc4ba802e3e017c35e863c0e4
Aaron Sutula <aaron.sutula@gmail.com>
2018-12-09 17:17:50 -0600

    Better logic for registering cafes

--- App/Sagas/NodeOnline.ts
+++ App/Sagas/NodeOnline.ts
@@ -13,11 +13,12 @@ export function * onNodeOnline () {
 
     // Make sure we've registered the Cafes
     const sessions: ReadonlyArray<CafeSession> = yield call(cafeSessions)
-    if (sessions.length < 1) {
-      const cafesString: string = Config.RN_TEXTILE_CAFES
-      const cafeIds = cafesString.split(',')
-      for (const cafeId of cafeIds) {
-        // TODO: Add some error handling here, maybe state management
+    const cafesString: string = Config.RN_TEXTILE_CAFES
+    const cafeIds = cafesString.split(',')
+    for (const cafeId of cafeIds) {
+      // TODO: Add some error handling here, maybe state management
+      const session = sessions.find((session) => session.cafe_id === cafeId)
+      if (!session) {
         yield call(registerCafe, cafeId)
       }
     }
