6f27b1cbbceeb972c5f91ae9633ab7e93dd60e60
andrew <andrewxhill@gmail.com>
2018-12-11 11:12:44 -0800

    moves nativemodule textilenode mock into own home

--- App/NativeModules/Textile/__tests__/Api.ts
+++ App/NativeModules/Textile/__tests__/Api.ts
@@ -1,51 +1,54 @@
 import {
     acceptExternalThreadInvite,
     acceptThreadInviteViaNotification,
-    addExternalThreadInvite
+    addExternalThreadInvite,
+    addSchema,
+    addThread,
+    addThreadFiles
 } from '../Api'
-import {
-    ExternalInvite
-} from '../Model'
+import { IDirectory } from '../pb/textile-go';
 
 const threadId = 'QmdNgTtH468cqZFzXCi4sVSWTbJMWQbhYb8cBVyikP9LzW'
 const threadKey = 'VsHHHz8bC8fu9k78RaX8ujQsUzGzaUxwKJyLFKKDacUZoWJaouGnzUQwgmh5'
-
-jest.mock('NativeModules', () => {
-    return {
-        TextileNode: {
-            acceptExternalThreadInvite: jest.fn((id, key): Promise<string> => new Promise((resolve) => {
-                resolve('SUCCESS')
-            })),
-            acceptThreadInviteViaNotification: jest.fn((id): Promise<string> => new Promise((resolve) => {
-                resolve('SUCCESS')
-            })),
-            addExternalThreadInvite: jest.fn((id): Promise<string> => new Promise((resolve) => {
-                const mockInvite: ExternalInvite = {
-                    id,
-                    key: 'VsHHHz8bC8fu9k78RaX8ujQsUzGzaUxwKJyLFKKDacUZoWJaouGnzUQwgmh5',
-                    inviter: 'mock'
-                }
-                resolve(JSON.stringify(mockInvite))
-            }))
-        }
-    }
-})
+const threadName = 'Great Name'
 
 describe('textile api', () => {
     describe('thread invites', () => {
-        it('acceptExternalThreadInvite', async () => {
+        it('accept external thread invite', async () => {
             const result = await acceptExternalThreadInvite(threadId, threadKey)
             expect(result).toEqual('SUCCESS')
         })
-        it('acceptThreadInviteViaNotification', async () => {
+        it('add thread invite via notification', async () => {
             const result = await acceptThreadInviteViaNotification(threadId)
             expect(result).toEqual('SUCCESS')
         })
-        it('addExternalThreadInvite', async () => {
+        it('add external thread invite', async () => {
             const result = await addExternalThreadInvite(threadId)
             expect(typeof result).toEqual('object')
             expect(result).toHaveProperty('id')
             expect(result.id).toEqual(threadId)
+        })
+    })
+    describe('threads', () => {
+        it('addSchema', async () => {
+            const result = await addSchema('{}')
+            expect(typeof result).toEqual('object')
+            expect(result).toHaveProperty('mill')
+        })
+        it('addThread', async () => {
+            const result = await addThread(threadKey, threadName)
+            expect(typeof result).toEqual('object')
+            expect(result).toHaveProperty('key')
+            expect(result.key).toEqual(threadKey)
+            expect(result).toHaveProperty('name')
+            expect(result.name).toEqual(threadName)
+        })
+        it('addThreadFiles', async () => {
+            const dir: IDirectory = {}
+            const result = await addThreadFiles(dir, threadId, 'here we go')
+            expect(typeof result).toEqual('object')
+            expect(result).toHaveProperty('thread_id')
+            expect(result.thread_id).toEqual(threadId)
         })
     })
 })
--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,14 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import TextileNode from './__mocks__/TextileNode' 
+import NavigationService from '../App/Services/NavigationService'
+
+// Custom mock APIs for native TextileNode
+jest.mock('NativeModules', () => {
+    return {
+        TextileNode
+    }
+})
 
 global.fetch = require('jest-fetch-mock')
 Enzyme.configure({ adapter: new Adapter() })
