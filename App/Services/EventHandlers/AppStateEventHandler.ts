cb4d4bd93af94325ea1c3c20007d6d9cd94b03ce
andrew <andrewxhill@gmail.com>
2019-01-10 17:20:15 -0800

    potential fix

--- App/Services/EventHandlers/AppStateEventHandler.ts
+++ App/Services/EventHandlers/AppStateEventHandler.ts
@@ -15,14 +15,24 @@ export default class AppStateEventHandler {
 
   handleAppState (nextState: AppStateStatus) {
     const currentState = this.store.getState().textileNode.appState
-    const newState: TextileAppStateStatus =  nextState === 'background' && (currentState === 'active' || currentState === 'inactive') ? 'backgroundFromForeground' : nextState
+    const newState: TextileAppStateStatus =  nextState === 'background' && (currentState === 'active' || currentState === 'inactive') ?'backgroundFromForeground' : nextState
     if (newState !== currentState || newState === 'background') {
       this.store.dispatch(TextileNodeActions.appStateChange(currentState, newState))
     }
   }
 
   setup () {
+    // capture whatever state already exists in the system
+    const initialState = AppState.currentState
+    // setup our listener for changes
     AppState.addEventListener('change', this.handleAppState.bind(this))
+    // grab our last stored state
+    const currentState = this.store.getState().textileNode.appState
+    if (initialState !== null && (initialState !== currentState || initialState === 'background')) {
+      // In cases where our listeners were setup after there was already a currentState
+      // if initialState === null we know we'll get our new state via the listener
+      this.handleAppState(initialState)
+    }
   }
 
   tearDown () {
