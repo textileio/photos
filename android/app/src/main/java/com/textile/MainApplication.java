6bf9cfb86c2996c30d1eb29a6c37f8f38db7de3b
andrew <andrewxhill@gmail.com>
2019-01-18 14:42:31 -0800

    first test

--- android/app/src/main/java/com/textile/MainApplication.java
+++ android/app/src/main/java/com/textile/MainApplication.java
@@ -3,6 +3,7 @@ package com.textile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.textile.screencontrol.RNScreenControlPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNScreenControlPackage(),
           new RNBackgroundFetchPackage(),
           new ReactNativePushNotificationPackage(),
           new UploaderReactPackage(),
