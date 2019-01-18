4b2f9e5c6690703e95760f933a69b5fed355d167
andrew <andrewxhill@gmail.com>
2019-01-18 14:58:19 -0800

    cleans up android

--- android/app/src/main/java/com/textile/MainApplication.java
+++ android/app/src/main/java/com/textile/MainApplication.java
@@ -37,7 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNScreenControlPackage(),
+          new RNScreenControlPackage(),
           new RNBackgroundFetchPackage(),
           new ReactNativePushNotificationPackage(),
           new UploaderReactPackage(),
