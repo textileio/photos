e33e406ec05c212ffe1210fe9189f64e000e9265
andrew <andrewxhill@gmail.com>
2019-01-20 10:05:45 -0800

    adding image back

---
+++ ios/NativeViews/TextileImageView.h
@@ -0,0 +1,18 @@
+#import <UIKit/UIKit.h>
+#import <React/RCTResizeMode.h>
+#import <React/UIView+React.h>
+
+@class RCTBridge;
+
+@interface TextileImageView : UIImageView
+
+- (instancetype)initWithBridge:(RCTBridge *)bridge NS_DESIGNATED_INITIALIZER;
+
+@property (nonatomic, strong) NSString *target;
+@property (nonatomic, assign) int index;
+@property (nonatomic, assign) int forMinWidth;
+@property (nonatomic, assign) RCTResizeMode resizeMode;
+@property (nonatomic, copy) RCTDirectEventBlock onLoad;
+@property (nonatomic, copy) RCTDirectEventBlock onError;
+
+@end
---
+++ ios/NativeViews/TextileImageView.m
@@ -0,0 +1,106 @@
+#import "TextileImageView.h"
+#import <React/RCTBridge.h>
+#import "TextileNode.h"
+
+@interface TextileImageView ()
+
+@property (nonatomic, assign) Boolean needsRenderResizeMode;
+@property (nonatomic, assign) Boolean needsRenderImage;
+
+@end
+
+@implementation TextileImageView {
+  // Weak reference back to the bridge, for image loading
+  __weak RCTBridge *_bridge;
+}
+
+RCT_NOT_IMPLEMENTED(- (instancetype)init)
+
+- (instancetype)initWithBridge:(RCTBridge *)bridge {
+  if ((self = [super init])) {
+    _bridge = bridge;
+    self.clipsToBounds = true;
+  }
+  return self;
+}
+
+- (void)setTarget:(NSString *)target {
+  if (_target != target) {
+    _target = target;
+  }
+  self.needsRenderImage = true;
+}
+
+- (void)setIndex:(int)index {
+  if (_index != index) {
+    _index = index;
+    self.needsRenderImage = true;
+  }
+}
+
+- (void)setForMinWidth:(int)forMinWidth {
+  if (_forMinWidth != forMinWidth) {
+    _forMinWidth = forMinWidth;
+    self.needsRenderImage = true;
+  }
+}
+
+- (void)setResizeMode:(RCTResizeMode)resizeMode {
+  if (_resizeMode != resizeMode) {
+    _resizeMode = resizeMode;
+    self.needsRenderResizeMode = true;
+  }
+}
+
+- (void)render {
+  if (self.needsRenderResizeMode) {
+    if (self.resizeMode == RCTResizeModeRepeat) {
+      // Repeat resize mode is handled by the UIImage. Use scale to fill
+      // so the repeated image fills the UIImageView.
+      self.contentMode = UIViewContentModeScaleToFill;
+    } else {
+      self.contentMode = (UIViewContentMode)(self.resizeMode);
+    }
+    self.needsRenderResizeMode = false;
+  }
+
+  if (self.needsRenderImage) {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+      NSError *error;
+      UIImage *image;
+      NSString *jsonString;
+      NSString *path = [NSString stringWithFormat:@"%@/%d", self.target, self.index];
+      jsonString = [self->_bridge.textileNode _imageFileDataForMinWidth:path minWidth:self.forMinWidth error:&error];
+      if (jsonString) {
+        NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error;
+        id json = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
+        NSDictionary *dict = (NSDictionary *)json;
+        NSString *urlString = [dict objectForKey:@"url"];
+        NSURL *url = [NSURL URLWithString:urlString];
+        NSData *imageData = [NSData dataWithContentsOfURL:url];
+        image = [UIImage imageWithData:imageData scale:1];
+      }
+      dispatch_async(dispatch_get_main_queue(), ^{
+        if (error) {
+          if (self.onError) {
+            self.onError(@{ @"message" : error.localizedDescription });
+          }
+        } else {
+          super.image = image;
+          if (self.onLoad) {
+            self.onLoad(@{});
+          }
+        }
+      });
+    });
+    self.needsRenderImage = false;
+  }
+}
+
+- (void)didSetProps:(NSArray<NSString *> *)changedProps {
+  [self render];
+}
+
+@end
+
