efebef08cbfc6bd66409367e0794a403dac13774
Andrew W. Hill <andrew@textile.io>
2019-02-08 11:16:43 -0800

    easily view redux state (#921)
    * easily view redux state

---
+++ App/SB/views/ReduxState/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { View, Text, Share, FlatList } from 'react-native'
+import { NavigationActions } from 'react-navigation'
+
+import { TextileHeaderButtons, Item as TextileItem } from '../../../Components/HeaderButtons'
+
+import styles from './statics/styles'
+
+class ReduxState extends React.PureComponent {
+  static navigationOptions = ({ navigation }) => {
+    const params = navigation.state.params || {}
+    return {
+      headerTitle: '',
+      headerLeft: (
+        <TextileHeaderButtons left>
+          <TextileItem title='Back' iconName='arrow-left' onPress={() => { navigation.dispatch(NavigationActions.back()) }} />
+        </TextileHeaderButtons>
+      ),
+      headerRight: ([
+        <TextileHeaderButtons right>
+          <TextileItem title='Share' onPress={params.share} />
+        </TextileHeaderButtons>
+      ])
+    }
+  }
+
+  componentDidMount () {
+    this.props.navigation.setParams({
+      share: () => { this._share() }
+    })
+  }
+
+  _share () {
+    Share.share({ title: '', message: this.props.redux })
+  }
+
+  renderRow ({ item }) {
+    return (
+      <Text>{item}</Text>
+    )
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.props.textLines}
+          renderItem={this.renderRow}
+          numColumns={1}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  const redux = JSON.stringify(state, undefined, 2)
+  return {
+    redux,
+    // instead of rendering big blob of text. https://github.com/facebook/react-native/issues/9077
+    textLines: redux.match(/[^\r\n]+/g)
+  }
+}
+
+export default connect(mapStateToProps, undefined)(ReduxState)
---
+++ App/SB/views/ReduxState/statics/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    marginTop: 20,
+    flex: 1
+  },
+  header: {
+    fontFamily: 'Biotif-Bold'
+  }
+})
