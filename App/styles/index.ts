ad648df05bb9ae154b8b72ac03003d9eef3090f4
Aaron Sutula <aaron.sutula@gmail.com>
2019-02-14 10:21:12 -0600

    sizing constants

--- App/styles/index.ts
+++ App/styles/index.ts
@@ -3,5 +3,6 @@ import * as fontFamily from './fontFamily'
 import * as fontSize from './fontSize'
 import * as textStyle from './textStyle'
 import * as spacing from './spacing'
+import * as size from './size'
 
-export { color, fontFamily, fontSize, textStyle, spacing }
+export { color, fontFamily, fontSize, textStyle, spacing, size }
