4f9bd17ae4d609ccb348474571366105d10884ff
Aaron Sutula <aaron.sutula@gmail.com>
2019-04-25 13:05:57 -0500

    Update a new file to new sdk api

--- App/Components/GroupCard/Members.tsx
+++ App/Components/GroupCard/Members.tsx
@@ -3,13 +3,13 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import Icon from '@textile/react-native-icon'
 import TextileImage from '../TextileImage'
 import Avatar from '../Avatar'
-import { pb } from '@textile/react-native-sdk'
+import { IContact } from '@textile/react-native-sdk'
 
 import styles, { cardImageStyle, ICON_WIDTH, ROW_COLUMN } from './statics/styles'
 import { color } from '../../styles'
 
 interface ScreenProps {
-  members: pb.IContact[]
+  members: IContact[]
 }
 
 class GroupMembers extends React.Component<ScreenProps> {
@@ -22,7 +22,7 @@ class GroupMembers extends React.Component<ScreenProps> {
 
     return (
       <View style={styles.avatarContainer}>
-        { members.slice(0, 8).map((mem: pb.IContact, i: number) => {
+        { members.slice(0, 8).map((mem: IContact, i: number) => {
           const imageStyle = cardImageStyle(members.length, i)
           return (
             <View key={mem.address} style={imageStyle} >
