7c1d4c509e46e399f3dd3497eb32d3f1b48bfb2b
Aaron Sutula <aaron.sutula@gmail.com>
2019-04-26 10:11:22 -0500

    Revert "Ignore SIGPIPE"
    This reverts commit d27b67b76dc800b938ec0aa2fa6e8daea57fa788.

--- ios/Textile/main.m
+++ ios/Textile/main.m
@@ -10,7 +10,6 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-  signal(SIGPIPE, SIG_IGN);
   @autoreleasepool {
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
