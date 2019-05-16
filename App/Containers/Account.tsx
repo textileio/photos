listenaddress/textile-mobile

Hide Photos from drawer if not verboseUi

App/Containers/Account.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { View, Text, ViewStyle } from 'react-native'
-import { DrawerItems, NavigationScreenProps, NavigationEvents } from 'react-navigation'
+import { DrawerItems, DrawerItemsProps } from 'react-navigation'
 import Icon from '@textile/react-native-icon'
 import Avatar from '../Components/Avatar'
 import { RootState } from '../Redux/Types'
@@ -14,9 +14,10 @@ const CONTAINER: ViewStyle = {
 
 interface StateProps {
   username: string
+  showPhotos: boolean
 }
 
-type Props = StateProps & NavigationScreenProps
+type Props = StateProps & DrawerItemsProps
 
 class Account extends Component<Props> {
   state = {  }
@@ -26,21 +27,24 @@ class Account extends Component<Props> {
   }
 
   render() {
+    const items = this.props.showPhotos ? this.props.items : this.props.items.filter((item) => item.key !== 'Photos')
+    const props = { ...this.props, items }
     return (
       <View style={CONTAINER}>
         <Icon style={{ position: 'absolute', top: 11, right: 11 }} color={color.grey_0} name='nut' onPress={this.navToSettings} size={24} />
         <View style={{ alignItems: 'center' }}>
           <Avatar style={{ width: 120, height: 120 }} self={true} />
-          <Text style={{ ...textStyle.header_l, margin: 20 }}>{this.props.username}</Text>
+          <Text style={{ ...textStyle.header_l, margin: 20 }}>{props.username}</Text>
         </View>
-        <DrawerItems {...this.props} />
+        <DrawerItems {...props} />
       </View>
     )
   }
 }
 
 const mapStateToProps = (state: RootState): StateProps => ({
-  username: state.account.profile.value ? state.account.profile.value.username || state.account.profile.value.id : 'unknown'
+  username: state.account.profile.value ? state.account.profile.value.username || state.account.profile.value.id : 'unknown',
+  showPhotos: state.preferences.verboseUi
 })
 
 export default connect(mapStateToProps, undefined)(Account)

textileio/photos

Signed-off-by: Aaron Sutula <hi@asutula.com>

App/Containers/Account.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { View, Text, ViewStyle } from 'react-native'
-import { DrawerItems, NavigationScreenProps, NavigationEvents, DrawerItemsProps } from 'react-navigation'
+import { DrawerItems, DrawerItemsProps } from 'react-navigation'
 import Icon from '@textile/react-native-icon'
 import Avatar from '../Components/Avatar'
 import { RootState } from '../Redux/Types'
@@ -14,6 +14,7 @@ const CONTAINER: ViewStyle = {
 
 interface StateProps {
   username: string
+  showPhotos: boolean
 }
 
 type Props = StateProps & DrawerItemsProps
@@ -26,21 +27,24 @@ class Account extends Component<Props> {
   }
 
   render() {
+    const items = this.props.showPhotos ? this.props.items : this.props.items.filter((item) => item.key !== 'Photos')
+    const props = { ...this.props, items }
     return (
       <View style={CONTAINER}>
         <Icon style={{ position: 'absolute', top: 11, right: 11 }} color={color.grey_0} name='nut' onPress={this.navToSettings} size={24} />
         <View style={{ alignItems: 'center' }}>
           <Avatar style={{ width: 120, height: 120 }} self={true} />
-          <Text style={{ ...textStyle.header_l, margin: 20 }}>{this.props.username}</Text>
+          <Text style={{ ...textStyle.header_l, margin: 20 }}>{props.username}</Text>
         </View>
-        <DrawerItems {...this.props} />
+        <DrawerItems {...props} />
       </View>
     )
   }
 }
 
 const mapStateToProps = (state: RootState): StateProps => ({
-  username: state.account.profile.value ? state.account.profile.value.name || state.account.profile.value.address : 'unknown'
+  username: state.account.profile.value ? state.account.profile.value.name || state.account.profile.value.address : 'unknown',
+  showPhotos: state.preferences.verboseUi
 })
 
 export default connect(mapStateToProps, undefined)(Account)