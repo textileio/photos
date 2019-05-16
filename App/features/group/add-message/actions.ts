18519c133e3956fa40eae634241b31494ed8aef6
Thomas Hobohm <public@thomashobohm.com>
2019-05-16 13:24:33 -0500

    Refactored some group code.
    Signed-off-by: Thomas Hobohm <public@thomashobohm.com>

--- App/features/group/add-message/actions.ts
+++ App/features/group/add-message/actions.ts
@@ -1,7 +1,20 @@
 import { createAsyncAction } from 'typesafe-actions'
 
+export interface addMessagePayload {
+  id: string
+  groupId: string
+}
+
+export interface addMessageRequestPayload extends addMessagePayload {
+  body: string
+}
+
+export interface addMessageFailurePayload extends addMessagePayload {
+  error: any
+}
+
 export const addMessage = createAsyncAction(
   'group/add-message/ADD_MESSAGE_REQUEST',
   'group/add-message/ADD_MESSAGE_SUCCESS',
   'group/add-message/ADD_MESSAGE_FAILURE'
-)<{ id: string, groupId: string, body: string }, { id: string, groupId: string }, { id: string, groupId: string, error: any }>()
+)<addMessageRequestPayload, addMessagePayload, addMessageFailurePayload>()
--- App/features/group/add-photo/reducer.ts
+++ App/features/group/add-photo/reducer.ts
@@ -1,4 +1,3 @@
-
 import { combineReducers } from 'redux'
 import { ActionType, getType } from 'typesafe-actions'
 
