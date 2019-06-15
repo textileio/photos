53a5d5e67004f9df860f4b476aefccee446fa49f
Thomas Hobohm <public@thomashobohm.com>
2019-06-15 15:14:20 -0400

    Add and link react-native-tab-view.
    Signed-off-by: Thomas Hobohm <public@thomashobohm.com>

--- android/app/src/main/java/com/textile/MainApplication.java
+++ android/app/src/main/java/com/textile/MainApplication.java
@@ -3,6 +3,7 @@ package com.textile;
 import android.support.multidex.MultiDexApplication;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
 import io.textile.cameraroll.RNCameraRollPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -40,6 +41,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
           new MainReactPackage(),
+            new ReanimatedPackage(),
             new RNCameraRollPackage(),
           new RNTextilePackage(),
           new RNBackgroundFetchPackage(),
