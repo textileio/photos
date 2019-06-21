4d6c95359425f75fbc845582a6da7f22ec760024
Aaron Sutula <hi@asutula.com>
2019-06-21 17:23:06 +0200

    Removing react-native-background-upload
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- typings/typings.d.ts
+++ typings/typings.d.ts
@@ -5,5 +5,4 @@ declare module '*.json' {
 
 declare module 'react-native-actionsheet'
 declare module 'react-native-pages'
-declare module 'react-native-background-upload'
 declare module 'react-native-hyperlink'
