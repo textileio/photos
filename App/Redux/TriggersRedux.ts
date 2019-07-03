d028edcd137ebf8afe4f4ec30979832087d8baf7
Aaron Sutula <hi@asutula.com>
2019-07-03 11:19:28 -0500

    remove location related code from ts files
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- App/Redux/TriggersRedux.ts
+++ App/Redux/TriggersRedux.ts
@@ -1,8 +1,7 @@
 import { createAction, ActionType, getType } from 'typesafe-actions'
 
 const actions = {
-  backgroundFetch: createAction('BACKGROUND_FETCH'),
-  locationUpdate: createAction('LOCATION_UPDATE')
+  backgroundFetch: createAction('BACKGROUND_FETCH')
 }
 
 export type TriggersAction = ActionType<typeof actions>
