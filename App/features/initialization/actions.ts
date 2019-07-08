45066295417841c8725444e275c4d34a48b78975
Thomas Hobohm <public@thomashobohm.com>
2019-07-08 15:20:53 -0400

    Prefix actions with initialization.
    Signed-off-by: Thomas Hobohm <public@thomashobohm.com>

--- App/features/initialization/actions.ts
+++ App/features/initialization/actions.ts
@@ -1,39 +1,39 @@
 import { createAction } from 'typesafe-actions'
 
 export const initializeNewAccount = createAction(
-  'INITIALIZE_CREATING_NEW_WALLET_AND_ACCOUNT',
+  'initialization/INITIALIZE_CREATING_NEW_WALLET_AND_ACCOUNT',
   resolve => {
     return () => resolve()
   }
 )
 
 export const initializeExistingAccount = createAction(
-  'INITIALIZE_WITH_EXISTING_ACCOUNT_SEED',
+  'initialization/INITIALIZE_WITH_EXISTING_ACCOUNT_SEED',
   resolve => {
     return (seed: string) => resolve({ seed })
   }
 )
 
-export const nodeInitialized = createAction('NODE_INITIALIZED', resolve => {
+export const nodeInitialized = createAction('initialization/NODE_INITIALIZED', resolve => {
   return () => resolve()
 })
 
 export const failedToInitializeNode = createAction(
-  'FAILED_TO_INITIALIZE_NODE',
+  'initialization/FAILED_TO_INITIALIZE_NODE',
   resolve => {
     return (error: any) => resolve({ error })
   }
 )
 
 export const chooseOnboardingPath = createAction(
-  'CHOOSE_ONBOARDING_PATH',
+  'initialization/CHOOSE_ONBOARDING_PATH',
   resolve => {
     return (path: string) => resolve({ path })
   }
 )
 
 export const setCurrentPage = createAction(
-  'SET_CURRENT_PAGE_ONBOARDING',
+  'initialization/SET_CURRENT_PAGE_ONBOARDING',
   resolve => {
     return (page: number) => resolve({ page })
   }
@@ -44,7 +44,7 @@ export const nextPage = createAction('NEXT_PAGE_ONBOARDING', resolve => {
 })
 
 export const onboardingSuccess = createAction(
-  'COMPLETE_ONBOARDING',
+  'initialization/COMPLETE_ONBOARDING',
   resolve => {
     return () => resolve()
   }
