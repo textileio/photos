bb0cc58ddbe61aea1194e59e84a18d56918603eb
Aaron Sutula <hi@asutula.com>
2019-08-05 17:31:45 -0500

    pass in project dir
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- scripts/download-config.sh
+++ scripts/download-config.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+PROJECT_DIR=$1
+
+echo "Project dir - $PROJECT_DIR"
+
 rm -rf /tmp/config-private
 git clone git@github.com:textileio/config-private.git /tmp/config-private
 
@@ -7,4 +11,4 @@ BUILD_TYPE=${RN_RELEASE_TYPE:-"dev"}
 
 echo "Build type - ${BUILD_TYPE}"
 
-cp -Rv /tmp/config-private/photos/${BUILD_TYPE}/* .
+cp -Rv /tmp/config-private/photos/${BUILD_TYPE}/* $PROJECT_DIR
