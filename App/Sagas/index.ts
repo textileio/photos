9acf3e20bfae597479f69d5eb263583d62d83758
Aaron Sutula <hi@asutula.com>
2019-08-08 11:16:59 -0500

    remove test event (#1288)
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- App/Sagas/index.ts
+++ App/Sagas/index.ts
@@ -102,23 +102,9 @@ function* waitForRehydrate() {
   }
 }
 
-function* logFirebase(event: string) {
-  yield call([Firebase.analytics(), 'logEvent'], event)
-}
-
-function* subscribeActions() {
-  while (true) {
-    const action: RootAction = yield take('*')
-    if (action.type === getType(TextileEventsActions.nodeStarted)) {
-      yield fork(logFirebase, 'tex_node_started')
-    }
-  }
-}
-
 export default function*() {
   yield call(waitForRehydrate)
   yield all([
-    call(subscribeActions),
     call(accountSaga),
     call(contactsSaga),
     call(groupSaga),
